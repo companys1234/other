f34efdaa836ffd01c5e1ebff2613b92c988af794
poco40 <128311098+companys1234@users.noreply.github.com>
2025-11-28 18:31:13 +0500

    Add files via upload

---
+++ random.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <random>
+
+int main()
+{
+    std::random_device rd;   
+    std::mt19937 gen(rd());
+    std::cout << "Hello World!\n";
+    int x;
+    std::cout << "Input number: ";
+    std::cin >> x;
+    std::cout << x << " Your num" << std::endl;
+    std::uniform_int_distribution<> dist1(1, 10);
+    int y = dist1(gen);
+    std::cout << y << " My num" << std::endl;
+    if (x == y) {
+        std::cout << "You win";
+    }
+    else {
+        std::cout << x - y << " Your error";
+    }
+    return 0;
+}
